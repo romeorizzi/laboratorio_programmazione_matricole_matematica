cd0429921d205f6626f5d3fc96a98c8d38b7b96b
alerighi <alessandro.righi@outlook.it>
2018-10-12 13:53:30 +0200

    Aggiornato script installazione ipython

--- setup/install_ipython.sh
+++ setup/install_ipython.sh
@@ -1,9 +1,13 @@
 #!/bin/bash
 
-pip3 install --user -U jupyter ipython==6 prompt_toolkit==1.0.15
+pip3 install --user -U ipython==6 prompt_toolkit==1.0.15
+pip3 install --user -U jupyter 
+pip3 install --user -U ipython==6 prompt_toolkit==1.0.15
 
 echo 'export PATH=$PATH:$HOME/.local/bin' >> ~/.bashrc
 
 source ~/.bashrc
 
-echo 'Inizia ad usare ipython digitando il comando ipython'   
+echo 'Installazione completata'
+echo 'Per utilizzare ipython digita il comando: ipython'
+echo 'Per avviare un notebook digita il comando: jupyter notebook &'
