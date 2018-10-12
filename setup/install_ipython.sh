181fabb3b226497f2bf074e88f08764d487e77da
alerighi <alessandro.righi@outlook.it>
2018-10-12 13:30:26 +0200

    Aggiunto script installazione ipython

---
+++ setup/install_ipython.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+pip3 install --user -U ipython==6 prompt_toolkit==1.0.15
+
+echo 'export PATH=$PATH:$HOME/.local/bin' >> ~/.bashrc
+
+source ~/.bashrc
+
+echo 'Inizia ad usare ipython digitando il comando ipython'   
